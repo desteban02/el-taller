3cdd8216a32c2bf4d50c6e8192386ce3d85ebbc9
unknown <daniel.esteban.lds@gmail.com>
2020-09-09 17:29:40 -0500

    modificacion> footer y javascript

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+function capturarDatos() {
+  console.log("Inicio de Funcion: Capturar datos");
+  /**
+   * Tipos de variables
+   * var, let, const
+   */
+  let txtNombre = document.getElementById("txtNombre");
+  let txtCorreo = document.getElementById("txtCorreo");
+  let txtMensaje = document.getElementById("txtMensaje");
+  console.log(txtNombre.value, txtCorreo.value , txtMensaje.value);
+ }
+
+ const capturarDatos2 = () => {
+  let txtNombre = document.getElementById("txtNombre");
+  let txtCorreo = document.getElementById("txtCorreo");
+  let txtMensaje = document.getElementById("txtMensaje");
+  console.log(txtNombre.value, txtCorreo.value , txtMensaje.value);
+ }
+
+ const metodos = {
+   capturarDatos3:() => { 
+    let txtNombre = document.getElementById("txtNombre");
+    let txtCorreo = document.getElementById("txtCorreo");
+    let txtMensaje = document.getElementById("txtMensaje");
+    let validacion = validarInformacion(txtCorreo.value, txtNombre.value, txtMensaje.value);
+    console.log("Validacion: ", validacion);
+   }
+    };
+
+ const validarInformacion = (correo, nombre, mensaje) =>{
+   if (correo.length > 0 && nombre.length > 0 &&  mensaje.length > 0) {
+     return true;
+   } else {
+     return false;
+   }
+ };
