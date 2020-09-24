e6b5b8bc0773850b350f4936044b305b19097950
unknown <daniel.esteban.lds@gmail.com>
2020-09-23 22:03:47 -0500

    Registro full con Css y Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,64 @@
+console.log("Desde un archivo.");
+
+/**
+ * Ejemplo de funcion
+ */
 function capturarDatos() {
-  console.log("Inicio de Funcion: Capturar datos");
+  console.log("Inicio de funcion:Capturar datos");
   /**
    * Tipos de variables
    * var, let, const
-   * a
    */
-  let txtNombre = document.getElementById("txtNombre");
   let txtCorreo = document.getElementById("txtCorreo");
+  let txtAsunto = document.getElementById("txtAsunto");
   let txtMensaje = document.getElementById("txtMensaje");
-  console.log(txtNombre.value, txtCorreo.value , txtMensaje.value);
- }
+  console.log(txtAsunto.value, txtMensaje.value, txtCorreo.value);
+}
 
- const capturarDatos2 = () => {
-  let txtNombre = document.getElementById("txtNombre");
+const capturarDatos2 = () => {
+  console.log("Inicio de funcion:Capturar datos");
   let txtCorreo = document.getElementById("txtCorreo");
+  let txtAsunto = document.getElementById("txtAsunto");
   let txtMensaje = document.getElementById("txtMensaje");
-  console.log(txtNombre.value, txtCorreo.value , txtMensaje.value);
- }
+  console.log(txtAsunto.value, txtMensaje.value, txtCorreo.value);
+};
 
- const metodos = {
-   capturarDatos3:() => { 
-    let txtNombre = document.getElementById("txtNombre");
+const metodos = {
+  capturarDatos3: () => {
     let txtCorreo = document.getElementById("txtCorreo");
+    let txtAsunto = document.getElementById("txtAsunto");
     let txtMensaje = document.getElementById("txtMensaje");
-    let validacion = validarInformacion(txtCorreo.value, txtNombre.value, txtMensaje.value);
-    console.log("Validacion: ", validacion);
-   }
-    };
+    console.log(txtAsunto.value, txtMensaje.value, txtCorreo.value);
 
- const validarInformacion = (correo, nombre, mensaje) =>{
-   if (correo.length > 0 && nombre.length > 0 &&  mensaje.length > 0) {
-     return true;
-   } else {
-     return false;
-   }
- };
+    let correo = txtCorreo.value;
+    let asunto = txtAsunto.value;
+    let mensaje = txtMensaje.value;
+
+    let validacion = validarInformacion(correo, asunto, mensaje);
+    if (validacion) {
+      console.log("Validación", validacion);
+      let contacto = {};
+      contacto.correo = correo;
+      contacto.asunto = asunto;
+      contacto.mensaje = mensaje;
+      //TODO: Pendiente de terminar el formulario de contacto con el backend.
+      console.log("JSON contacto", contacto);
+    } else {
+      alert("Datos incorrectos.");
+    }
+  },
+};
+
+const validarInformacion = (correo, asunto, mensaje) => {
+  //Opcion if
+  //   if (correo.length > 0 && asunto.length > 0 && mensaje.length > 0) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+
+  //   Opcion ternario
+  //   return correo.length > 0 && asunto.length > 0 && mensaje.length > 0 ? true : false;
+
+  return correo.length > 0 && asunto.length > 0 && mensaje.length > 0;
+};
